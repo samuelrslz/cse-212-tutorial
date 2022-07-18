ae4034a93dc140d3f79840cf25dffc4e76e9ab81
Samuel Rios-Lazo <83593415+samuelrslz@users.noreply.github.com>
2022-07-18 16:52:47 -0600

    Update tree_challenge_solved.py

--- code/tree_challenge_solved.py
+++ code/tree_challenge_solved.py
@@ -1,7 +1,7 @@
 tree = ['-', ['*', 13, ['+', ['/', 32, -4], 12]], ['*', ['+', ['-', 10, 1], 3], 4]]
 
 def evaluate_tree(t):
-    if isinstance(t, int): 
+    if isinstance(t, int):   # Check if it's an integer so it can return the answer.
         return t
     elif t[0]=="-":
         return evaluate_tree(t[1]) - evaluate_tree(t[2])
